847f1121a5513b7d1db3fe558ca36bf65a601c71
olinator2013 <93063569+olinator2013@users.noreply.github.com>
2024-01-21 13:27:37 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(1, 6))
+})
+input.onButtonPressed(Button.AB, function () {
+    max = 0
+    basic.showNumber(max)
+})
+input.onButtonPressed(Button.B, function () {
+    max += 1
+    basic.showNumber(max)
+})
+let max = 0
+max = 6
